63c9170f98d838c768938db7befcd9d7491e4f9c
Josuelee <josuelee28@gmail.com>
2022-07-06 14:33:21 -0600

    initial structure

---
+++ src/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite"
+
+import { getAuth } from "firebase/auth"
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "netflix-clone-e4c54.firebaseapp.com",
+  projectId: "netflix-clone-e4c54",
+  storageBucket: "netflix-clone-e4c54.appspot.com",
+  messagingSenderId: "1012443284713",
+  appId: "1:1012443284713:web:53b6d30b3d624f04c75c78"
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+
+
+export { auth }
+export default db
